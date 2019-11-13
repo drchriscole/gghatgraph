c1149fdca9217d97ecd66981518feddb92d3935c
Chris Cole <c.cole@dundee.ac.uk>
2019-11-13 16:47:39 +0000

    works like for like as example with exemplar data. Adding TODOs and comments

--- ggplot_version.R
+++ ggplot_version.R
@@ -1,6 +1,7 @@
 
 library(ggplot2)
 
+# TODO - make it work with other data
 dt <- data.frame(subjectID = seq(1,30),DV = rnorm(30,50,20),IV1 = rep(c(1,2),15), IV2 = c(rep(1,15),rep(2,15)))
 xLab <- "Independent Variable 2"
 yLab <- "Dependent Variable"
@@ -15,39 +16,27 @@ dtMeans$SEM <- dtSEM$DV / sqrt(length(unique(dt$subjectID)))
 dtMeans$hatPos <- as.integer(as.factor(dtMeans$IV1))
 dtMeans$xPos <- as.integer(as.factor(dtMeans$IV2))
 grandMean <- mean(dtMeans$DV)
+# TODO - if the inter-group means are quite different and
+#      - and the intra-group variance is large +/- the grandSD
+#      - may not be a enough of a range to represent the data
 grandSD <- mean(dtSEM$DV) * .75 # produces y-axis range of 1.5 SDs (see Witt (in press) for details)
 
-
 ggplot(dtMeans, aes(x = factor(IV2), y = DV, group=hatPos )) + 
-  geom_errorbar(aes(ymax = DV, ymin = DV), position = position_dodge(0.4), width = 0.4) +
+  # first, plot means. Using geom_errorbar() as couldn't see another way to draw a line
+  geom_errorbar(aes(ymax = DV, ymin = DV), position = position_dodge(0.4), width = 0.4, size = 0.8) +
+  # next, add the rectangle representing the within-group difference
+  # TODO - make this generic
   geom_rect( xmin = c(NA,1,NA,2) + 0,
              xmax = c(NA,1,NA,2) + 0.2,
              ymin = c(rep(dtMeans[1,3],2),rep(dtMeans[3,3],2)), 
              ymax = c(dtMeans[1:2,3],dtMeans[3:4,3]),
+             size = 0.8,
              colour = 'black') +
-  ylim(c(grandMean - grandSD, grandMean + grandSD))
+  # finally, add the actual error bar
+  geom_errorbar(aes(ymax = DV-SEM, ymin = DV+SEM), position = position_dodge(0.4), width = 0.03, colour = "darkgrey") +
+  # set the y-axis range to 
+  ylim(c(grandMean - grandSD, grandMean + grandSD)) +
+  theme_minimal() +
+  theme(panel.grid = element_blank(),
+        axis.line = element_line())
 
-
-  
-#  scratch space #
-#
-#   geom_rect( xmin = as.numeric(dtMeans$IV2) - 0.3,
-#             xmax = as.numeric(dtMeans$IV2) + 0.3,
-#             ymin = c(rep(dtMeans[1,3],2),rep(dtMeans[3,3],2)), 
-#             ymax = c(dtMeans[1:2,3],dtMeans[3:4,3]),
-#             linetype = 'dashed'
-#             )
-#   
-#   geom_point(shape = '-', size = 4)
-#   layer(geom = 'point', stat = 'identity', params = list(shape = '-', size = 4))
-# 
-# 
-# df <- data.frame(x = c("a","a","b","b"), y = c(2,1.5,1.2,1.8), pos = c(1,2,1,2))
-# ggplot(data = df, aes(x=x, y=y, group=pos)) +
-#   geom_errorbar(aes(ymax = y, ymin = y), position = position_dodge(0.4), width = 0.4)
-#   geom_col() +
-#   geom_rect(data = df, aes(x = x, y=y), xmin = as.numeric(df$x[[2]]) - 0.2,
-#             xmax = as.numeric(df$x[[3]]) + 0.3,
-#             ymin = 1.5, ymax = 2,
-#             linetype = 'dashed')
-  
